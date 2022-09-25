1a37dbfbf50d5c33a7ae2d81409765aac2f42dc1
Sunil Reddy <sunilreddy@Sunils-MacBook-Air.local>
2022-09-25 13:58:25 +0530

    create a saparate compponet for todos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import Todo from "./components/Todo";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>MY TODOS</h1>
+      <Todo text="react1" />
+      <Todo text="react2" />
+      <Todo text=" react3" />
     </div>
   );
 }
 
 export default App;
+
+{
+  /* <div className="App">
+<header className="App-header">
+  <img src={logo} className="App-logo" alt="logo" />
+  <p>welcome to my website</p>
+  <a
+    className="insta-link"
+    href="https://www.instagram.com/i_sunil_reddy/"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    instagram
+  </a>
+  <a
+    className="linkedin"
+    href="https://www.linkedin.com/in/sunilreddy-chinthaparthi/"
+    target="blank"
+  >
+    linkedin
+  </a>
+</header>
+</div> */
+}
